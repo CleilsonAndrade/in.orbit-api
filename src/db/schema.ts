adc0a84318656dedf0628a9a1c541e641a9d9832
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-09-11 20:24:56 -0300

    feat(create goal): resource created and configurated validation body

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
 import { createId } from '@paralleldrive/cuid2'
 import { int, mysqlTable, timestamp, varchar } from 'drizzle-orm/mysql-core'
 
-export const goal = mysqlTable('tb_goal', {
+export const goals = mysqlTable('tb_goal', {
   id: varchar('id', { length: 255 })
     .primaryKey()
     .unique()
@@ -11,13 +11,13 @@ export const goal = mysqlTable('tb_goal', {
   createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
 })
 
-export const goalCompletions = mysqlTable('tb_goal_completions', {
+export const goalsCompletions = mysqlTable('tb_goal_completions', {
   id: varchar('id', { length: 255 })
     .primaryKey()
     .unique()
     .$defaultFn(() => createId()),
   goalId: varchar('goal_id', { length: 255 })
-    .references(() => goal.id)
+    .references(() => goals.id)
     .notNull(),
   createdAt: timestamp('created_at', { mode: 'date' }).notNull().defaultNow(),
 })
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,44 @@
 import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import z from 'zod'
+import { createGoal } from '../functions/create-goal'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
+
+const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3333
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
-    console.log('Server running: http://localhost:3333')
+    console.log(`Server running: \nhttp://localhost:${port}`)
   })
